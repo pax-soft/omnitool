fb1dedcecfe0dbbab260cc24cf225ccd2a2f1953
Piotr Czarnecki <pioczarn@gmail.com>
2024-07-07 14:44:21 +0200

    Fix optional feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+#[cfg(feature = "bit-vec-omnitool")]
 pub use bit_vec_omnitool as bit_vec;
+
+#[cfg(feature = "bit-set-omnitool")]
 pub use bit_set_omnitool as bit_set;
+
+#[cfg(feature = "earley-omnitool")]
 pub use earley_omnitool as earley;
