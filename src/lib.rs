07f4a417f60f3eda55900441a24572d39337c67e
Piotr Czarnecki <pioczarn@gmail.com>
2024-07-07 15:57:28 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,17 @@
+#[cfg(feature = "earley-omnitool")]
+pub extern crate earley_omnitool;
+
+#[cfg(feature = "earley-omnitool")]
+pub use earley_omnitool as earley;
+
+#[cfg(feature = "bit-vec-omnitool")]
+pub extern crate bit_vec_omnitool;
+
 #[cfg(feature = "bit-vec-omnitool")]
 pub use bit_vec_omnitool as bit_vec;
 
 #[cfg(feature = "bit-set-omnitool")]
-pub use bit_set_omnitool as bit_set;
+pub extern crate bit_set_omnitool;
 
-#[cfg(feature = "earley-omnitool")]
-pub use earley_omnitool as earley;
+#[cfg(feature = "bit-set-omnitool")]
+pub use bit_set_omnitool as bit_set;
